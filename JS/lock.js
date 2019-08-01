c9d95e4e721a3cff87a20ba306cca43f1817f5ac
Luca Negrette <lucanegrette@hotmail.com>
2019-08-01 20:27:01 -0300

    faca

--- JS/lock.js
+++ JS/lock.js
@@ -3,4 +3,7 @@ var body = document.getElementsByTagName("body")[0];
 localStorage.swi ? console.log("permitido") :  body.innerHTML = "<b>Acesso Negado</b>";
 
 
+var elDocument = document.documentElement;
+
+elDocument.className = elDocument.className.replace(/(^|\s)no-js(\s|$)/, '$1');
 
